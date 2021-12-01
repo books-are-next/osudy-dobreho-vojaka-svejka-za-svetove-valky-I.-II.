cd1e421754dfdd6390e3a4a14cfa190bc5ae7e01
jan-martinek <honza.martinek@gmail.com>
2021-12-01 15:58:03 +0000

    Deploying to docs from @ books-are-next/osudy-dobreho-vojaka-svejka-za-svetove-valky-I.-II.@f2738d89599fa54c83f424bd3f9dcbccfd969658 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 /* global self, caches, fetch */
 /* eslint-disable no-restricted-globals */
 
-const CACHE = 'cache-f4abf7d';
+const CACHE = 'cache-f2738d8';
 
 self.addEventListener('install', e => {
   e.waitUntil(precache()).then(() => self.skipWaiting());
@@ -39,7 +39,7 @@ self.addEventListener('activate', event => {
 });
 
 function precache() {
-  return caches.open(CACHE).then(cache => cache.addAll(["./","./colophon.html","./favicon.png","./index.html","./manifest.json","./osudy_dobreho_vojaka_svejka_i_ii_001.html","./osudy_dobreho_vojaka_svejka_i_ii_002.html","./osudy_dobreho_vojaka_svejka_i_ii_005.html","./osudy_dobreho_vojaka_svejka_i_ii_006.html","./osudy_dobreho_vojaka_svejka_i_ii_007.html","./osudy_dobreho_vojaka_svejka_i_ii_008.html","./osudy_dobreho_vojaka_svejka_i_ii_009.html","./osudy_dobreho_vojaka_svejka_i_ii_010.html","./osudy_dobreho_vojaka_svejka_i_ii_011.html","./osudy_dobreho_vojaka_svejka_i_ii_012.html","./osudy_dobreho_vojaka_svejka_i_ii_013.html","./osudy_dobreho_vojaka_svejka_i_ii_014.html","./osudy_dobreho_vojaka_svejka_i_ii_015.html","./osudy_dobreho_vojaka_svejka_i_ii_016.html","./osudy_dobreho_vojaka_svejka_i_ii_018.html","./osudy_dobreho_vojaka_svejka_i_ii_019.html","./osudy_dobreho_vojaka_svejka_i_ii_017.html","./osudy_dobreho_vojaka_svejka_i_ii_020.html","./osudy_dobreho_vojaka_svejka_i_ii_021.html","./osudy_dobreho_vojaka_svejka_i_ii_022.html","./osudy_dobreho_vojaka_svejka_i_ii_023.html","./osudy_dobreho_vojaka_svejka_i_ii_024.html","./osudy_dobreho_vojaka_svejka_i_ii_025.html","./osudy_dobreho_vojaka_svejka_i_ii_026.html","./osudy_dobreho_vojaka_svejka_i_ii_027.html","./osudy_dobreho_vojaka_svejka_i_ii_028.html","./resources.html","./resources/image001_fmt.jpeg","./resources/image003_fmt.jpeg","./resources/image004_fmt.jpeg","./resources/index.xml","./resources/obalka_osudy_dobreho_v_fmt.jpeg","./resources/upoutavka_eknihy_fmt.jpeg","./scripts/bundle.js","./style/style.min.css"]));
+  return caches.open(CACHE).then(cache => cache.addAll(["./","./colophon.html","./favicon.png","./index.html","./manifest.json","./osudy_dobreho_vojaka_svejka_i_ii_001.html","./osudy_dobreho_vojaka_svejka_i_ii_002.html","./osudy_dobreho_vojaka_svejka_i_ii_005.html","./osudy_dobreho_vojaka_svejka_i_ii_006.html","./osudy_dobreho_vojaka_svejka_i_ii_007.html","./osudy_dobreho_vojaka_svejka_i_ii_008.html","./osudy_dobreho_vojaka_svejka_i_ii_009.html","./osudy_dobreho_vojaka_svejka_i_ii_010.html","./osudy_dobreho_vojaka_svejka_i_ii_011.html","./osudy_dobreho_vojaka_svejka_i_ii_012.html","./osudy_dobreho_vojaka_svejka_i_ii_013.html","./osudy_dobreho_vojaka_svejka_i_ii_014.html","./osudy_dobreho_vojaka_svejka_i_ii_015.html","./osudy_dobreho_vojaka_svejka_i_ii_016.html","./osudy_dobreho_vojaka_svejka_i_ii_017.html","./osudy_dobreho_vojaka_svejka_i_ii_018.html","./osudy_dobreho_vojaka_svejka_i_ii_019.html","./osudy_dobreho_vojaka_svejka_i_ii_020.html","./osudy_dobreho_vojaka_svejka_i_ii_021.html","./osudy_dobreho_vojaka_svejka_i_ii_022.html","./osudy_dobreho_vojaka_svejka_i_ii_023.html","./osudy_dobreho_vojaka_svejka_i_ii_024.html","./osudy_dobreho_vojaka_svejka_i_ii_025.html","./osudy_dobreho_vojaka_svejka_i_ii_026.html","./osudy_dobreho_vojaka_svejka_i_ii_027.html","./osudy_dobreho_vojaka_svejka_i_ii_028.html","./resources.html","./resources/image001_fmt.jpeg","./resources/image003_fmt.jpeg","./resources/image004_fmt.jpeg","./resources/index.xml","./resources/obalka_osudy_dobreho_v_fmt.jpeg","./resources/upoutavka_eknihy_fmt.jpeg","./scripts/bundle.js","./style/style.min.css"]));
 }
 
 self.addEventListener('fetch', e => {
