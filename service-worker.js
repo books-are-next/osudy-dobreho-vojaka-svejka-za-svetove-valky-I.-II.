fcd1b9e7505c88bb6d64662912bc212d42def57b
jan-martinek <jan-martinek@users.noreply.github.com>
2022-10-07 14:16:14 +0000

    Deploying to docs from @ books-are-next/osudy-dobreho-vojaka-svejka-za-svetove-valky-I.-II.@22c8380da9d527f2a09f3effd99c636c4038f457 🚀

--- service-worker.js
+++ service-worker.js
@@ -764,7 +764,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /// <reference lib="WebWorker" />
 require("regenerator-runtime");
 const sw = self;
-const CACHE = 'cache-6543088';
+const CACHE = 'cache-22c8380';
 /**
  * IndexedDB singleton wrapper used to store persistent information with an predefined {@link Schema}
  */
